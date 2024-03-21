b5efa3be5ae237a3495e5b2359cc6725afcc6bf4
chari00 <macharitocruzuk@gmail.com>
2024-03-21 09:37:34 +0000

    add home button.

--- app/Navigations/MenuBarDesktop.js
+++ app/Navigations/MenuBarDesktop.js
@@ -3,11 +3,20 @@ import Link from "next/link"
 
 export default function MenuBarDesktop() {
     return (
+        <div className="MenuBar">
+
+        <section>
+            <Link href="/">
+                <button>Home</button>
+            </Link>
+        </section>
         <section>
             <Link href="/founders">
                 <button>Meet the founders</button>
             </Link>
         </section>
+        </div>
+
     );
 }
 
--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
     
-    <main className="main">
+    <main id="main">
       <Hero/>
       <Card/>
      
