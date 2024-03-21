2b6617bbcc403ba771a38ffe2da8b5744c4d1423
chari00 <macharitocruzuk@gmail.com>
2024-03-21 23:45:08 +0000

    add images to the mobile menuBar.

--- app/src/menuBarMobile/MenuBarMobile.jsx
+++ app/src/menuBarMobile/MenuBarMobile.jsx
@@ -1,8 +1,17 @@
+import Image from "next/image"
+import menu from '../images/foundersImage/Mobile-Menu-Open.png';
+import openButton from '../images/foundersImage/menu-open-button.png';
+import closeButton from '../images/foundersImage/menu-close-button.png';
+
+
 
 export default function MenuBarMobile (){
     return (
     <div>
-        
+        <Image className="openBtn" src={openButton} alt="button"/>
+        <Image className="menuBtn" src={menu} alt="button"/>
+        <Image className="closeBtn" src={closeButton} alt="button"/>
+       
     </div>
    )
 }
